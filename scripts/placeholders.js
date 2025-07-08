7e3b13e5b3e76785b3dd730dde2c88ef542b44a4
Ketan Sharma <kesharma@adobe.com>
2025-07-08 12:24:21 +0530

    commit

--- scripts/placeholders.js
+++ scripts/placeholders.js
@@ -1,62 +1,30 @@
 // eslint-disable-next-line import/prefer-default-export
-/* Auxiliary script migrated from source */
-// eslint-disable-next-line import/prefer-default-export
-
-import { toCamelCase } from './aem.js';
-
-// eslint-disable-next-line import/prefer-default-export
-
-// eslint-disable-next-line import/prefer-default-export
 export async function fetchPlaceholders(prefix = 'default') {
-// eslint-disable-next-line import/prefer-default-export
   window.placeholders = window.placeholders || {};
-// eslint-disable-next-line import/prefer-default-export
   if (!window.placeholders[prefix]) {
-// eslint-disable-next-line import/prefer-default-export
     window.placeholders[prefix] = new Promise((resolve) => {
-// eslint-disable-next-line import/prefer-default-export
       fetch(`${prefix === 'default' ? '' : prefix}/placeholders.json`)
-// eslint-disable-next-line import/prefer-default-export
         .then((resp) => {
-// eslint-disable-next-line import/prefer-default-export
           if (resp.ok) {
-// eslint-disable-next-line import/prefer-default-export
             return resp.json();
-// eslint-disable-next-line import/prefer-default-export
           }
-// eslint-disable-next-line import/prefer-default-export
           return {};
-// eslint-disable-next-line import/prefer-default-export
-        }).then((json) => {
-// eslint-disable-next-line import/prefer-default-export
+        })
+        .then((json) => {
           const placeholders = {};
-// eslint-disable-next-line import/prefer-default-export
           json.data
-// eslint-disable-next-line import/prefer-default-export
             .filter((placeholder) => placeholder.Key)
-// eslint-disable-next-line import/prefer-default-export
             .forEach((placeholder) => {
-// eslint-disable-next-line import/prefer-default-export
               placeholders[toCamelCase(placeholder.Key)] = placeholder.Text;
-// eslint-disable-next-line import/prefer-default-export
             });
-// eslint-disable-next-line import/prefer-default-export
           window.placeholders[prefix] = placeholders;
-// eslint-disable-next-line import/prefer-default-export
           resolve(window.placeholders[prefix]);
-// eslint-disable-next-line import/prefer-default-export
-        }).catch(() => {
-// eslint-disable-next-line import/prefer-default-export
+        })
+        .catch(() => {
           window.placeholders[prefix] = {};
-// eslint-disable-next-line import/prefer-default-export
           resolve(window.placeholders[prefix]);
-// eslint-disable-next-line import/prefer-default-export
         });
-// eslint-disable-next-line import/prefer-default-export
     });
-// eslint-disable-next-line import/prefer-default-export
   }
-// eslint-disable-next-line import/prefer-default-export
-  return window.placeholders[`${prefix}`];
-// eslint-disable-next-line import/prefer-default-export
+  return window.placeholders[prefix];
 }
