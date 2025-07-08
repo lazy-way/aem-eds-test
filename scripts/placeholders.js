7d6771cccd2521f4dd38d3b067decbda55485e70
Ketan Sharma <kesharma@adobe.com>
2025-07-08 11:46:41 +0530

    commit

--- scripts/placeholders.js
+++ scripts/placeholders.js
@@ -1,28 +1,33 @@
-export async function fetchPlaceholders(prefix = 'default') {
-  window.placeholders = window.placeholders || {};
-  if (!window.placeholders[prefix]) {
-    window.placeholders[prefix] = new Promise((resolve) => {
-      fetch(`${prefix === 'default' ? '' : prefix}/placeholders.json`)
-        .then((resp) => {
-          if (resp.ok) {
-            return resp.json();
-          }
-          return {};
-        }).then((json) => {
-          const placeholders = {};
-          json.data
-            .filter((placeholder) => placeholder.Key)
-            .forEach((placeholder) => {
-              placeholders[toCamelCase(placeholder.Key)] = placeholder.Text;
-            });
-          window.placeholders[prefix] = placeholders;
-          resolve(window.placeholders[prefix]);
-        }).catch(() => {
-          // error loading placeholders
-          window.placeholders[prefix] = {};
-          resolve(window.placeholders[prefix]);
-        });
-    });
-  }
-  return window.placeholders[`${prefix}`];
+/* Placeholder script to fetch dynamic placeholders */ 
+import { toCamelCase } from './aem.js'; 
+
+export async function fetchPlaceholders(prefix = "default") { 
+  window.placeholders = window.placeholders || {}; 
+
+  if (!window.placeholders[prefix]) { 
+    window.placeholders[prefix] = new Promise((resolve) => { 
+      fetch(`${prefix === "default" ? "" : prefix}/placeholders.json`) 
+        .then((resp) => { 
+          if (resp.ok) { 
+            return resp.json(); 
+          } 
+          return {}; 
+        }) 
+        .then((json) => { 
+          const placeholders = {}; 
+          json.data 
+            .filter((placeholder) => placeholder.Key) 
+            .forEach((placeholder) => { 
+              placeholders[toCamelCase(placeholder.Key)] = placeholder.Text; 
+            }); 
+          window.placeholders[prefix] = placeholders; 
+          resolve(window.placeholders[prefix]); 
+        }) 
+        .catch(() => { 
+          window.placeholders[prefix] = {}; 
+          resolve(window.placeholders[prefix]); 
+        }); 
+    }); 
+  } 
+  return window.placeholders[prefix]; 
 }
